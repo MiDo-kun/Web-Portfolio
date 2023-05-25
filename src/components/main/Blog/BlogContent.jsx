852420b52196d67e0717d888d0ca1f09fdc0a37c
Jerson Dela Cerna <72369026+MiDo-kun@users.noreply.github.com>
2023-05-26 06:38:32 +0800

    Changed website icon

--- src/components/main/Blog/BlogContent.jsx
+++ src/components/main/Blog/BlogContent.jsx
@@ -11,21 +11,21 @@ const BlogContent = ({ _id, title, cover, content, createdAt }) => {
   const datePosted = new Date(createdAt).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
 
   return (
-    <div className="flex p-4 bg-slate-950 rounded-md mobile-lg:py-3 mobile-lg:px-3">
+    <div className="flex p-3 bg-gray-200 rounded-md mobile-lg:py-2 mobile-lg:mx-1">
       <div className="w-1/3">
         <a href={`${BLOG_SITE}/post/${_id}`} target="_blank" rel="noopener noreferrer">
           <img src={cover} alt={title} />
         </a>
       </div>
       <div className="flex pl-3 flex-col justify-between w-2/3">
-        <h2 className="text-[1rem] font-medium text-blue-400 hover:underline mobile-lg:text-[.88rem] mobile-sm:text-[.8rem]">
-          <a href={`${BLOG_SITE}/post/${_id}`} target="_blank" rel="noopener noreferrer">
+        <h2 className="text-[1rem] font-semibold text-blue-700 hover:underline mobile-md:text-sm mobile-xs:text-xs">
+          <a id="blog-title" href={`${BLOG_SITE}/post/${_id}`} target="_blank" rel="noopener noreferrer">
             {title}
           </a>
         </h2>
-        <div className="flex gap-3">
-          <time className="text-xs text-gray-400 mobile-md:text-[.75rem]">{datePosted}</time>
-          <span className="text-xs text-gray-400 mobile-md:text-[.75rem]">{readingTime(content)} min. read</span>
+        <div className="flex gap-3 font-medium">
+          <time className="text-xs  text-gray-600 mobile-md:text-[.71rem]">{datePosted}</time>
+          <span className="text-xs  text-gray-600 mobile-md:text-[.71rem]">{readingTime(content)} min. read</span>
         </div>
       </div>
     </div>
