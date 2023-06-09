29fbd1d0b74f71c42dbd7fb1b6d0473e9401dc6f
Jerson Dela Cerna <smidorima781@gmail.com>
2023-06-09 14:54:01 +0000

    Changed wesbite name

--- src/components/footer/Signature.jsx
+++ src/components/footer/Signature.jsx
@@ -2,7 +2,7 @@ const Signature = () => {
   const currentYear = new Date().getFullYear();
   return (
     <p className="px-5 py-2 text-center text-sm text-gray-200 bg-slate-950 lg:text-xs">
-      Designed and Developed by <span className="text-blue-400">Jerson</span>. <br className="hidden mobile-lg:block" /> All Rights Reserved &copy; <span className="text-blue-400">{currentYear}.</span>
+      Designed and Developed by <span className="text-blue-400">Jerson</span>. <br className="hidden mobile-lg:block" /> All Rights Reserved &copy; <span className="text-blue-400">{currentYear}</span>.
     </p>
   )
 }
