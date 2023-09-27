bc6d3d8533e9c3fe65de6927f71176955acc8ee4
MiDo-kun <smidorima781@gmail.com>
2023-09-27 20:33:36 +0800

    Fixed: Blog posts fetch error

--- src/components/main/Blog/BlogContainer.jsx
+++ src/components/main/Blog/BlogContainer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import BlogContent from './BlogContent';
 
 const BlogContainer = () => {
-  const [BlogInfo, setBlogInfo] = useState([]);
+  const [blogInfo, setBlogInfo] = useState([]);
   const BLOG_ENDPOINT = import.meta.env.VITE_BLOG_ENDPOINT;
 
   useEffect(() => {
@@ -10,16 +10,17 @@ const BlogContainer = () => {
   }, []);
 
   const fetchBlogs = async () => {
-    const response = await fetch(BLOG_ENDPOINT + '/posts');
+    const response = await fetch(BLOG_ENDPOINT + '/posts?page=0&limit=6');
     const jsonData = await response.json();
-    setBlogInfo(jsonData);
+    const blogs = jsonData.posts;
+    setBlogInfo(blogs);
   }
 
   return (
     <div className="grid gap-7 grid-cols-2 -ml-5 laptop-sm:grid-cols-2 tablet-lg:grid-cols-1 mobile-lg:gap-5 mobile-lg:ml-0">
-      {!BlogInfo.length && <p className="text-xs text-gray-200">Fetching Blogs...</p>}
+      {!blogInfo.length && <p className="text-xs text-gray-200">Fetching Blogs...</p>}
       {
-        BlogInfo.slice(0, 8).map((blog, index) => (
+        blogInfo.map((blog, index) => (
           <div key={index} className={`tablet-lg:${index < 5 ? 'block' : 'hidden'}`}>
             <BlogContent {...blog} />
           </div>
